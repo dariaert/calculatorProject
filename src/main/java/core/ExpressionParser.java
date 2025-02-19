97332ebb382a8a1ab51d802a331ea835f3ad784e
vtoroipolz <dary.mo@yandex.ru>
2025-02-19 14:39:01 +0700

    History of operations

--- src/main/java/core/ExpressionParser.java
+++ src/main/java/core/ExpressionParser.java
@@ -1,4 +1,23 @@
 package main.java.core;
 
+import java.util.Map;
+
 public class ExpressionParser {
+
+    private final Map<Character, Operation> operations;
+    private final Memory memory;
+
+    public ExpressionParser(Map<Character, Operation> operations, Memory memory) {
+        this.operations = operations;
+        this.memory = memory;
+    }
+
+    private int precedence(char op) {
+        return switch (op){
+            case '+', '-' -> 1;
+            case '*', '/' -> 2;
+            case '^' -> 3;
+            default -> -1;
+        };
+    }
 }
--- src/main/java/core/Memory.java
+++ src/main/java/core/Memory.java
@@ -1,4 +1,28 @@
 package main.java.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Memory {
+
+    private final HashMap<String, Double> variables = new HashMap<>();
+    private final List<String> history = new ArrayList<>();
+
+    public void saveResult(String expression, double result){
+        history.add(expression + " = " + result);
+    }
+
+    public List<String> getHistory(){
+        return history;
+    }
+
+    public void setVariables(String name, double value){
+        variables.put(name, value);
+    }
+
+    public double getVariables(String name){
+        return variables.getOrDefault(name, null);
+    }
+
 }
